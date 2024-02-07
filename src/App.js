3f5ba7f0be8514617e9f7c5da551653347e68d7c
Kashyap Bavadiya <bavadiyakashyap@gmail.com>
2024-02-07 15:25:04 +0530

    HomePage Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button } from "@mui/material"
+import Header from "./components/Header"
+import Slider from "./components/Slider"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Home"
+import PageNotFound from "./components/PageNotFound"
+import Service from "./components/Service"
+import Signup from "./components/serviceProvider/Signup"
+import Login from "./components/serviceProvider/Login"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/service" element={<Service />} />
+					<Route path="/providerlogin" element={<Login />} />
+					<Route path="/providersignup" element={<Signup />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</Router>
+		</>
+	)
 }
 
-export default App;
+export default App
