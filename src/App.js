18514305492806631bc276636703462e7ac46883
Kashyap Bavadiya <bavadiyakashyap@gmail.com>
2024-02-17 02:24:26 +0530

    Login & Signup Completed for Provider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import PageNotFound from "./components/PageNotFound"
 import Service from "./components/Service"
 import Signup from "./components/serviceProvider/Signup"
 import Login from "./components/serviceProvider/Login"
+import ProviderHome from "./components/serviceProvider/ProviderHome"
+import EmailTaken from "./components/forgetPassword/EmailTaken"
+import VerifyOTP from "./components/forgetPassword/VerifyOTP"
+import PasswordReset from "./components/forgetPassword/PasswordReset"
+import Recovered from "./components/forgetPassword/Recovered"
 
 function App() {
 	return (
@@ -14,8 +19,13 @@ function App() {
 			<Router>
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/providerhome" element={<ProviderHome />} />
 					<Route path="/service" element={<Service />} />
 					<Route path="/providerlogin" element={<Login />} />
+					<Route path="/providerlogin/forget" element={<EmailTaken />} />
+					<Route path="/providerlogin/otpverify" element={<VerifyOTP />} />
+					<Route path="/providerlogin/resetpass" element={<PasswordReset />} />
+					<Route path="/providerlogin/recovered" element={<Recovered />} />
 					<Route path="/providersignup" element={<Signup />} />
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
